0e7c0a4e41b936de506d6a5599b96b0eb71f8c79
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-01-06 16:04:48 +0100

    Use multilevel thresholding to select the character
    Otsu's method reduces a greylevel image to a binary image by
    bruteforcing the best threshold based on intra-class variance. This
    method can be adapted to generate n-ary images. As bruteforcing all
    combinations of thresholds is quite expensive, the maximum acceptable
    number of thresholds is very small.
    See "A Fast Algorithm for Multilevel Thresholding" by Ping-Sung Liao,
    Tse-Sheng Chen and Pau-Choo Chung for details.

--- main.hs
+++ main.hs
@@ -5,12 +5,41 @@ import Vision.Primitive
 import qualified Vision.Histogram as H
 import qualified System.Console.Terminal.Size as T
 import System.Console.CmdTheLine
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 
 fileName :: Term String
 fileName = required $ pos 0 Nothing posInfo {posName = "FILENAME", posDoc = "path to the file to be converted to ascii"}
 
 argWidth :: Term (Maybe Int)
 argWidth = value $ opt Nothing (optInfo ["w", "width"]) {optName = "WIDTH", optDoc = "Width of the output in characters"}
+
+addTo :: a -> [[a]] -> [[a]]
+addTo x = fmap (x:)
+
+thresholds :: Int -> Int -> [[Int]]
+thresholds 1 m = fmap (:[]) [m+1 .. 254]
+thresholds n m = concatMap (\x -> addTo x (thresholds (n-1) x)) [m+1 .. 255-n]
+
+toRange :: [Int] -> [(Int, Int)]
+toRange ( [v]  ) = [(v, 255)]
+toRange (u:v:xs) = (u, v-1) : toRange (v:xs)
+
+sigma :: H.Histogram DIM1 Double -> [Int] -> Double
+sigma hist thresh = sum $ replaceNaN $ fmap (uncurry (interClassVariance)) $ toRange thresh
+    where
+        histVec = H.vector hist
+        interClassVariance u v = s u v ^ (2::Integer) / p u v
+        replaceNaN = map (\x -> if isNaN x then 0 else x)
+        lS = (sumVec V.!)
+        multVec = V.imap (\i x -> fromIntegral (i+1) * x) histVec
+        sumVec = V.postscanl (+) 0 multVec
+        lP = (V.postscanl (+) 0 histVec V.!)
+        p u v = lP v - lP u
+        s u v = lS v - lS u
+
+multiOtsu :: H.Histogram DIM1 Double -> Int -> [Int]
+multiOtsu hist n = fst $ maximumBy (comparing snd) $ fmap (\x -> (x, sigma hist (0:x))) (thresholds n 1)
 
 fitToWidth :: Int -> RGB -> RGB
 fitToWidth width img = resize TruncateInteger (ix2 height width) img
@@ -29,29 +58,15 @@ imgHeight img = imgHeight' $ shape img
         imgHeight' (Z :. h :. _) = h
 
 asciiChars :: [Char]
-asciiChars = "█▓▒░#%@+*:,,., "
-
-divideEvenly :: Double -> [Int] -> [[Int]]
-divideEvenly _    [] = [[]]
-divideEvenly size xs = bucket : (divideEvenly size remaining)
-    where
-        (bucket, remaining) = (take n xs, drop n xs)
-        n = n' $ dropWhile (\l -> fromIntegral (sum (take l xs)) < size) [1 .. length xs]
-        n' [] = length xs
-        n' (x:_) = x
-
-replaceWith :: a -> [b] -> [a]
-replaceWith e l = map (const e) l
-
-asciiHist :: H.Histogram DIM1 Int -> [Char]
-asciiHist hist = concat $ zipWith replaceWith asciiChars balancedHist
-    where
-       histList (H.Histogram _ vec) = V.toList vec
-       bucketSize = (fromIntegral $ sum (histList hist)) / (fromIntegral $ length asciiChars) :: Double
-       balancedHist = divideEvenly bucketSize (histList hist)
+asciiChars = "█▓▒░"
 
 pixelToAscii :: GreyPixel -> H.Histogram DIM1 Int -> Char
-pixelToAscii pixel hist = asciiHist hist !! (fromIntegral pixel)
+pixelToAscii pix hist = pixelToAscii' (multiOtsu gnorm 3) asciiChars
+    where
+        pixelToAscii' :: [Int] -> [Char] -> Char
+        pixelToAscii' [] (c:_) = c
+        pixelToAscii' (x:xs) (c:cs) = if fromIntegral pix < x then c else pixelToAscii' xs cs
+        gnorm = H.normalize 1.0 hist
 
 toAsciiRow :: Grey -> Int -> String
 toAsciiRow img y = fmap (\x -> pixelToAscii (index img (Z:. y :. x)) (H.histogram Nothing img :: H.Histogram DIM1 Int)) [0 .. (imgWidth img) - 1]
