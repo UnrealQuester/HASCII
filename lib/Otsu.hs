ad14cdcb668ed6909fe4f6c2ccb2655c1de2776f
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-07-31 20:16:25 +0200

    Refactor multiOtsu to use two auxillary functions

--- lib/Otsu.hs
+++ lib/Otsu.hs
@@ -11,12 +11,16 @@ range :: Int -> [(Int, Int)] -> [[(Int, Int)]]
 range 0 xs = [xs]
 range n xs = concatMap (range (n-1) . splitRange xs) ((rangePair . head) xs)
 
+pixelRange :: Int -> V.Vector [(Int, Int)]
+pixelRange n = V.fromList $ range n [(0, 255)]
+
 rangePair :: (Int, Int) -> [Int]
 rangePair p = [fst p .. snd p - 1]
 
 splitRange :: [(Int, Int)] -> Int -> [(Int, Int)]
 splitRange [] _ = []
 splitRange (x:xs) n = (fst x, n) : (n + 1, snd x) : xs
+
 
 sigma :: V.Vector Double -> [(Int, Int)] -> Double
 sigma histVec thresh = sum $ map (uncurry interClassVariance) thresh
@@ -30,5 +34,8 @@ sigma histVec thresh = sum $ map (uncurry interClassVariance) thresh
         p  u v = lP V.! v - lP V.! u
         s  u v = sumVec V.! v - sumVec V.! u
 
+bestCandidate :: Ord b => (a -> b) -> V.Vector a -> a
+bestCandidate f xs = fst $ V.maximumBy (comparing snd) $ V.map (id &&& f) xs
+
 multiOtsu :: V.Vector Double -> Int -> [Int]
-multiOtsu histVec n = fst $ V.maximumBy (comparing snd) $ V.map ((<$>) snd &&& sigma histVec) (V.fromList $ range n [(0, 255)])
+multiOtsu histVec n = snd <$> bestCandidate (sigma histVec) (pixelRange n)
